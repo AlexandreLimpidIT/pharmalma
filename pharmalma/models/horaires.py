33d41f1b3b70e8af0398ec2a72a70e04ccb06ec7
aomaridir00 <aomar.idir@ecoles-epsi.net>
2024-10-09 15:00:26 +0200

    les models

--- pharmalma/models/horaires.py
+++ pharmalma/models/horaires.py
@@ -6,7 +6,7 @@ class Horiares(models.Model):
     id_pharma=models.ForeignKey(Pharmacies,on_delete=models.CASCADE)
     jour=models.CharField(max_length=8)
     periode=models.CharField(max_length=5)
-    h_debut=models.FloatField()
-    h_fin=models.FloatField()
+    h_debut=models.TimeField()
+    h_fin=models.TimeField()
     class Meta:
         db_table="Horaires"
